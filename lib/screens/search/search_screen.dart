363715e180c6bce757ef6e0ff85b1c205956be90
JackWH <yukiakira259@gmail.com>
2022-10-09 18:01:20 +0700

    added things

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sizer/sizer.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 2, vertical: 16),
+              child: Container(
+                height: 8.h,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(color: Colors.orange)),
+                    hintText: 'Tra cứu luật',
+                  ),
+                ),
+              ),
+            )
+          ]),
+    );
   }
 }
