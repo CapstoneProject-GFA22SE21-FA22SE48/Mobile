9d61deddb307598f2faa093518832e035393efd0
JackWH <yukiakira259@gmail.com>
2022-11-21 15:09:06 +0700

    added moar stuffs

--- lib/screens/search/law/search_law_detail.dart
+++ lib/screens/search/law/search_law_detail.dart
@@ -252,9 +252,44 @@ class _SearchLawDetailScreen extends State<SearchLawDetailScreen> {
                                                                 null &&
                                                             !cc.laws.contains(widget
                                                                 .searchLawDto)) {
-                                                          print(cc.laws.length);
                                                           cc.laws.add(widget
                                                               .searchLawDto!);
+                                                          if (!Get
+                                                              .isSnackbarOpen) {
+                                                            Get.snackbar(
+                                                                'Thành công',
+                                                                'Đã thêm vào biên bản',
+                                                                colorText:
+                                                                    Colors
+                                                                        .green,
+                                                                backgroundColor:
+                                                                    Colors
+                                                                        .white,
+                                                                duration:
+                                                                    Duration(
+                                                                        seconds:
+                                                                            2),
+                                                                isDismissible:
+                                                                    true);
+                                                          }
+                                                        } else {
+                                                          if (!Get
+                                                              .isSnackbarOpen) {
+                                                            Get.snackbar(
+                                                                'Cảnh báo',
+                                                                'Điều này đã có trong biên bản',
+                                                                colorText: Colors
+                                                                    .redAccent,
+                                                                backgroundColor:
+                                                                    Colors
+                                                                        .white,
+                                                                duration:
+                                                                    Duration(
+                                                                        seconds:
+                                                                            2),
+                                                                isDismissible:
+                                                                    true);
+                                                          }
                                                         }
                                                       },
                                                       label: const Text(
